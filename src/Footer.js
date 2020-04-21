314947b15f28c75c56138d7d41fb32ab0c813a79
Tina <ipochinili@gmail.com>
2020-04-21 11:55:21 +0300

    new react app

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <p>Copyrite</p>
+      </footer>
+    )
+  }
+}
+export default Footer;
